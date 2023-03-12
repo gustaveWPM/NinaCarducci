34a2656d082a42d6173bfb6b2beae3a9d5c72f7d
gustaveWPM <jgeorge@perso.be>
2023-03-12 22:39:13 +0100

    add: removed JQuery

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,2 +1,34 @@
-$(document).ready((function(){$(".gallery").mauGallery({columns:{xs:1,sm:2,md:3,lg:3,xl:3},lightBox:!0,lightboxId:"myAwesomeLightbox",showTags:!0,tagsPosition:"top"})}));
-function f(){const es=document.querySelectorAll(".nav-item .nav-link"),i=document.querySelector("#hamburger-killswitch"),x=new bootstrap.Collapse(i,{toggle:!1});es.forEach(e=>{e.addEventListener("click",()=>{x.toggle()})})}f(),f=void 0;
+document.addEventListener("DOMContentLoaded", () => {
+    function runMauGallery() {
+        const opt = {
+            columns: {
+                xs: 1,
+                sm: 2,
+                md: 3,
+                lg: 3,
+                xl: 3
+            },
+            lightBox: true,
+            lightboxId: 'myAwesomeLightbox',
+            showTags: true,
+            tagsPosition: 'top'
+        };
+        mauGallery(opt);
+    }
+
+    function collapseNavbarOnClick() {
+        const navLinks = document.querySelectorAll('.nav-item .nav-link')
+        const menuToggle = document.querySelector('#navbar-hamburger-killswitch')
+        const bsCollapse = new bootstrap.Collapse(menuToggle, { toggle: false })
+        const hamburgerWidthPxBreakpoint = 767
+        navLinks.forEach(l => {
+            l.addEventListener('click', () => {
+                if (window.innerWidth <= hamburgerWidthPxBreakpoint) {
+                    bsCollapse.toggle()
+                }
+            })
+        })
+    }
+    runMauGallery();
+    collapseNavbarOnClick();
+});
